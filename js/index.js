a12484326a275a1579e76be1b02f753bedc97b6e
Ahmed Adisa <adisa.ahmed881@gmail.com>
2022-10-21 00:34:03 +0100

    trying to achive banner change onload

--- js/index.js
+++ js/index.js
@@ -1,7 +1,19 @@
-var imageNo = Math.floor(
-  Math.random() * 3 + 1
-); /* Change the 5 to the amount of images you have */
-$(".main_body_content_header").attr(
-  "src",
-  "bannerimage-" + imageNo + ".jpg"
-); /* Images must be named bannerimage-1.jpg, bannerimage-2.jpg, etc or any convention of your choosing*/
+const imgs = [
+  "vent2.jpg",
+  "vent3.jpg",
+  "vent4.jpg",
+  "vent5.jpg",
+  "vent6.jpg",
+  "vent7.jpg",
+  "vent8.jpg",
+  "vent9.jpg",
+  "vent10.jpg",
+];
+
+window.onload = function () {
+  const random_index = Math.floor(math.random() * imgs.length);
+
+  selected_img = imgs[random_index];
+
+  document.getElementById(main_body_content_header) = `./img/${selected_img}`;
+};
